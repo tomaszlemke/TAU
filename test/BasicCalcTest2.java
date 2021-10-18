96d5a5ca6fe8ee19130f5b1df527fb0f4a07dba0
Tomasz Lemke <tom@MacBook-Pro-Tomasz.local>
2021-10-18 20:40:39 +0200

    First 9 tests

---
+++ test/BasicCalcTest2.java
@@ -0,0 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BasicCalcTest2 {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void add() {
+    }
+
+    @Test
+    public void sub() {
+    }
+}
