d32947a1c85fc4840b94985ad74199d850b9df17
Tomasz Lemke <s19471@pjwstk.edu.pl>
2021-12-01 14:02:48 +0100

    both tests done for both browesers

--- cw2/main.py
+++ cw2/main.py
@@ -4,7 +4,6 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
 import logging
-import time
 
 logger = logging.getLogger('simple_example')
 logger.setLevel(logging.INFO)
@@ -72,63 +71,144 @@ logger.addHandler(ch)
 #
 # driverFireFox.close()
 
-driverChrome = webdriver.Chrome(executable_path='C:/Users/u6071468/PycharmProjects/TAU/chromedriver.exe')
+#--------------------------------------------------------------------------------------------------------------
+
+# driverChrome = webdriver.Chrome(executable_path='C:/Users/u6071468/PycharmProjects/TAU/chromedriver.exe')
+#
+# logger.info('Przechodzę na stronę Sklepu NewBalance')
+# driverChrome.get('https://nbsklep.pl/')
+#
+# try:
+#     temp = WebDriverWait(driverChrome, 30).until(
+#         EC.element_to_be_clickable((By.XPATH,
+#                                     '/html/body/app-root/app-menu/app-menu-mobile-newbalance/app-cookies-newbalance/div/div/div/div[2]/div/div[2]/span[2]')))
+#     temp.click()
+#
+# finally:
+#     logger.info('Cookies done')
+#
+# try:
+#     temp = WebDriverWait(driverChrome, 10).until(
+#         EC.presence_of_element_located((By.XPATH, '//*[@id="popupContainer"]/div[1]/img')))
+#     temp.click()
+# finally:
+#     logger.info('Newsletter exit')
+#
+# try:
+#     temp = WebDriverWait(driverChrome, 10).until(
+#         EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-menu/app-menu-mobile-newbalance/ul/li[2]/a[1]')))
+#     temp.click()
+# finally:
+#     logger.info('Otwieram Search bar')
+#
+# try:
+#     temp = WebDriverWait(driverChrome, 10).until(
+#         EC.element_to_be_clickable((By.XPATH,
+#                                     '/html/body/app-root/app-menu/app-menu-mobile-newbalance/div/div[2]/div/app-searcher-default/form/input')))
+#     temp.click()
+#
+# finally:
+#     logger.info('Focus na search bar')
+#
+# try:
+#     temp = WebDriverWait(driverChrome, 10).until(
+#         EC.element_to_be_clickable((By.XPATH,
+#                                     '/html/body/app-root/app-menu/app-menu-mobile-newbalance/div/div[2]/div/app-searcher-default/form/input')))
+#     temp.send_keys('ML574HA2')
+#
+# finally:
+#     logger.info('Wprowadzam kod')
+#
+# try:
+#     temp = WebDriverWait(driverChrome, 10).until(
+#         EC.element_to_be_clickable((By.XPATH,
+#                                     '/html/body/app-root/app-menu/app-menu-mobile-newbalance/div/div[2]/div/app-searcher-default/button/span[1]')))
+#     temp.click()
+#
+# finally:
+#     logger.info('Szukam')
+#
+# try:
+#     temp = WebDriverWait(driverChrome, 10).until(
+#         EC.element_to_be_clickable((By.XPATH,
+#                                     '/html/body/app-root/app-content-page-newbalance/div/div/ul[2]/li[1]/div/div/div[2]/a/div[1]/nb-img/img')))
+#     temp.click()
+#
+# finally:
+#     logger.info('Otwieram okno produktu')
+#
+# try:
+#     temp = WebDriverWait(driverChrome, 10).until(EC.presence_of_element_located((By.XPATH,
+#                                                                                  '/html/body/app-root/app-product-newbalance/div[2]/div[1]/div[1]/div[2]/app-product-form-newbalance/form/ul/li[1]/div/app-price-default/div/div/em/b')))
+#     cena = temp.text
+# finally:
+#     logger.info('Pobralem cene produktu')
+#
+# cena = cena[:-3]
+# if int(cena) < 200:
+#     logger.info('Kupuj')
+# else:
+#     logger.info('Poczekaj az bedzie taniej')
+
+#------------------------------------------------------------------------------------------------------------
+
+driverFireFox = webdriver.Firefox(executable_path='C:/Users/u6071468/PycharmProjects/TAU/geckodriver.exe')
 
 logger.info('Przechodzę na stronę Sklepu NewBalance')
-driverChrome.get('https://nbsklep.pl/')
+driverFireFox.get('https://nbsklep.pl/')
 
 try:
-    temp = WebDriverWait(driverChrome, 10).until(
+    temp = WebDriverWait(driverFireFox, 20).until(
         EC.element_to_be_clickable((By.XPATH,
-                                    '/html/body/app-root/app-menu/app-menu-mobile-newbalance/app-cookies-newbalance/div/div/div/div[2]/div/div[2]/span[2]')))
+                                    '/html/body/app-root/app-menu/app-menu-desktop-newbalance/div[2]/div[2]/app-cookies-newbalance/div/div/div/div[2]/div/div[2]/span[2]')))
     temp.click()
 
 finally:
     logger.info('Cookies done')
 
 try:
-    temp = WebDriverWait(driverChrome, 10).until(
+    temp = WebDriverWait(driverFireFox, 10).until(
         EC.presence_of_element_located((By.XPATH, '//*[@id="popupContainer"]/div[1]/img')))
     temp.click()
 finally:
     logger.info('Newsletter exit')
 
 try:
-    temp = WebDriverWait(driverChrome, 10).until(
-        EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-menu/app-menu-mobile-newbalance/ul/li[2]/a[1]')))
+    temp = WebDriverWait(driverFireFox, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-menu/app-menu-desktop-newbalance/div[2]/div[2]/app-header-newbalance/div/div/div[1]/app-searcher-default/form/input')))
     temp.click()
 finally:
     logger.info('Otwieram Search bar')
 
 try:
-    temp = WebDriverWait(driverChrome, 10).until(
+    temp = WebDriverWait(driverFireFox, 10).until(
         EC.element_to_be_clickable((By.XPATH,
-                                    '/html/body/app-root/app-menu/app-menu-mobile-newbalance/div/div[2]/div/app-searcher-default/form/input')))
+                                    '/html/body/app-root/app-menu/app-menu-desktop-newbalance/div[2]/div[2]/app-header-newbalance/div/div/div[1]/app-searcher-default/form/input')))
     temp.click()
 
 finally:
     logger.info('Focus na search bar')
 
 try:
-    temp = WebDriverWait(driverChrome, 10).until(
+    temp = WebDriverWait(driverFireFox, 10).until(
         EC.element_to_be_clickable((By.XPATH,
-                                    '/html/body/app-root/app-menu/app-menu-mobile-newbalance/div/div[2]/div/app-searcher-default/form/input')))
+                                    '/html/body/app-root/app-menu/app-menu-desktop-newbalance/div[2]/div[2]/app-header-newbalance/div/div/div[1]/app-searcher-default/form/input')))
     temp.send_keys('ML574HA2')
 
 finally:
     logger.info('Wprowadzam kod')
 
 try:
-    temp = WebDriverWait(driverChrome, 10).until(
+    temp = WebDriverWait(driverFireFox, 10).until(
         EC.element_to_be_clickable((By.XPATH,
-                                    '/html/body/app-root/app-menu/app-menu-mobile-newbalance/div/div[2]/div/app-searcher-default/button/span[1]')))
+                                    '/html/body/app-root/app-menu/app-menu-desktop-newbalance/div[2]/div[2]/app-header-newbalance/div/div/div[1]/app-searcher-default/button/span[1]')))
     temp.click()
 
 finally:
     logger.info('Szukam')
 
 try:
-    temp = WebDriverWait(driverChrome, 10).until(
+    temp = WebDriverWait(driverFireFox, 10).until(
         EC.element_to_be_clickable((By.XPATH,
                                     '/html/body/app-root/app-content-page-newbalance/div/div/ul[2]/li[1]/div/div/div[2]/a/div[1]/nb-img/img')))
     temp.click()
@@ -137,7 +217,7 @@ finally:
     logger.info('Otwieram okno produktu')
 
 try:
-    temp = WebDriverWait(driverChrome, 10).until(EC.presence_of_element_located((By.XPATH,
+    temp = WebDriverWait(driverFireFox, 10).until(EC.presence_of_element_located((By.XPATH,
                                                                                  '/html/body/app-root/app-product-newbalance/div[2]/div[1]/div[1]/div[2]/app-product-form-newbalance/form/ul/li[1]/div/app-price-default/div/div/em/b')))
     cena = temp.text
 finally:
