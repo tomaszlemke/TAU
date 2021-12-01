5cfccd7ef9c463020050e4f3187a4000be34a904
Tomasz Lemke <s19471@pjwstk.edu.pl>
2021-12-01 12:34:33 +0100

    try/finally + webdriver wait implemented

--- cw2/main.py
+++ cw2/main.py
@@ -1,5 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 import logging
 import time
@@ -74,39 +76,73 @@ driverChrome = webdriver.Chrome(executable_path='C:/Users/u6071468/PycharmProjec
 
 logger.info('Przechodzę na stronę Sklepu NewBalance')
 driverChrome.get('https://nbsklep.pl/')
-time.sleep(4)
-temp = driverChrome.find_element(By.CLASS_NAME, 'btn--cookies')
-time.sleep(2)
-temp.click()
-time.sleep(2)
 
-# temp = driverChrome.find_element(By.XPATH, '/html/body/app-root/app-menu/app-menu-mobile-newbalance/ul/li[2]/a[1]')
-# temp.click()
-# time.sleep(2)
+try:
+    temp = WebDriverWait(driverChrome, 10).until(
+        EC.element_to_be_clickable((By.XPATH,
+                                    '/html/body/app-root/app-menu/app-menu-mobile-newbalance/app-cookies-newbalance/div/div/div/div[2]/div/div[2]/span[2]')))
+    temp.click()
 
-temp = driverChrome.find_element(By.XPATH, '//*[@id="popupContainer"]/div[1]/img')
-time.sleep(2)
-temp.click()
+finally:
+    logger.info('Cookies done')
 
-temp = driverChrome.find_element(By.CLASS_NAME, 'searcher-input')
-time.sleep(2)
-temp.click()
-time.sleep(1)
+try:
+    temp = WebDriverWait(driverChrome, 10).until(
+        EC.presence_of_element_located((By.XPATH, '//*[@id="popupContainer"]/div[1]/img')))
+    temp.click()
+finally:
+    logger.info('Newsletter exit')
 
-temp.send_keys('ML574HA2')
+try:
+    temp = WebDriverWait(driverChrome, 10).until(
+        EC.element_to_be_clickable((By.XPATH, '/html/body/app-root/app-menu/app-menu-mobile-newbalance/ul/li[2]/a[1]')))
+    temp.click()
+finally:
+    logger.info('Otwieram Search bar')
 
-temp = driverChrome.find_element(By.CLASS_NAME, 'icon-search')
-time.sleep(2)
-temp.click()
-time.sleep(4)
+try:
+    temp = WebDriverWait(driverChrome, 10).until(
+        EC.element_to_be_clickable((By.XPATH,
+                                    '/html/body/app-root/app-menu/app-menu-mobile-newbalance/div/div[2]/div/app-searcher-default/form/input')))
+    temp.click()
 
-temp = driverChrome.find_element(By.XPATH, '/html/body/app-root/app-content-page-newbalance/div/div/ul[2]/li[1]/div/div/div[2]/a/div[1]/nb-img/img')
-time.sleep(2)
-temp.click()
-time.sleep(4)
+finally:
+    logger.info('Focus na search bar')
 
-temp = driverChrome.find_element(By.XPATH, '/html/body/app-root/app-product-newbalance/div[2]/div[1]/div[1]/div[2]/app-product-form-newbalance/form/ul/li[1]/div/app-price-default/div/div/em/b')
-cena = temp.text
+try:
+    temp = WebDriverWait(driverChrome, 10).until(
+        EC.element_to_be_clickable((By.XPATH,
+                                    '/html/body/app-root/app-menu/app-menu-mobile-newbalance/div/div[2]/div/app-searcher-default/form/input')))
+    temp.send_keys('ML574HA2')
+
+finally:
+    logger.info('Wprowadzam kod')
+
+try:
+    temp = WebDriverWait(driverChrome, 10).until(
+        EC.element_to_be_clickable((By.XPATH,
+                                    '/html/body/app-root/app-menu/app-menu-mobile-newbalance/div/div[2]/div/app-searcher-default/button/span[1]')))
+    temp.click()
+
+finally:
+    logger.info('Szukam')
+
+try:
+    temp = WebDriverWait(driverChrome, 10).until(
+        EC.element_to_be_clickable((By.XPATH,
+                                    '/html/body/app-root/app-content-page-newbalance/div/div/ul[2]/li[1]/div/div/div[2]/a/div[1]/nb-img/img')))
+    temp.click()
+
+finally:
+    logger.info('Otwieram okno produktu')
+
+try:
+    temp = WebDriverWait(driverChrome, 10).until(EC.presence_of_element_located((By.XPATH,
+                                                                                 '/html/body/app-root/app-product-newbalance/div[2]/div[1]/div[1]/div[2]/app-product-form-newbalance/form/ul/li[1]/div/app-price-default/div/div/em/b')))
+    cena = temp.text
+finally:
+    logger.info('Pobralem cene produktu')
+
 cena = cena[:-3]
 if int(cena) < 200:
     logger.info('Kupuj')
